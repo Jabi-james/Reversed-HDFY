9dc847cdb5138408f5cf5e149aa7aa3aa5a860f5
Jabi-james <91572300+Jabi-james@users.noreply.github.com>
2025-01-10 13:48:50 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ addEventListener(
        const ip = event.request.headers.get('cf-connecting-ip') || event.request.headers.get('x-forwarded-for') || (event.request.socket && event.request.socket.remoteAddress);
        let url = new URL(event.request.url);
        const worker_domain=url.hostname;
-       url.hostname = "";                        
+       url.hostname = "aws.jabist.ggff.net";                        
        url.protocol = event.request.headers.get('x-forwarded-proto') || "https";
        let request = new Request(url, event.request);
        if (ip)
